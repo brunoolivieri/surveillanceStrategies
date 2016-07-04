5c551fd803a7e9fb0c16e04815c10058cb3c556d
brunoolivieri <bruno@olivieri.com.br>
2016-07-04 18:12:56 -0300

    Merging benchmark version with devel version.

--- linuxRunners/testaTodos.sh
+++ linuxRunners/testaTodos.sh
@@ -17,19 +17,19 @@ do
 	do
 
 	for ((i=1; i<=LOOPS; i++)); do
+
+		echo " "
+		echo " ----------------------------------------------------------------------------"
+		echo " "
+		echo " nPOI = "$N_POI"  strat = "$Sname"  i = "$i 
+		date >> acompanhamento.txt
+		echo "nPOI = "$N_POI"  strat = "$Sname"  i = "$i >> acompanhamento.txt
+		echo " "
+		echo " ----------------------------------------------------------------------------"
+		echo " "
   
 		java -cp binaries/bin/. sinalgo.Run -project UAV_Surveillance -rounds $ROUNDS -refreshRate $REFRESHRATE -batch exitAfter=true exitAfter/Rounds=$ROUNDS exitOnTerminationInGUI=true AutoStart=true outputToConsole=false extendedControl=false -gen $N_UAV UAV_Surveillance:UAVnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=UAV_Surveillance:$Sname R=ReliableDelivery -gen 1 UAV_Surveillance:GSnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery -gen $N_POI UAV_Surveillance:POInode UAV_Surveillance:PoiDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery
 		
-		echo " "
-		echo " ----------------------------------------------------------------------------"
-		echo " "
-		echo " nUAV = "$N_UAV"  strat = "$Sname"  i = "$i 
-		echo " "
-		echo " ----------------------------------------------------------------------------"
-		echo " "
-
-
-
 	done
 
 	done
