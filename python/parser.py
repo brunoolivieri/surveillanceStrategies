c04901ed4aa1add3987895270bd72ff965049436
JustAnOtherGuy <jaog@lac2.inf>
2016-12-08 11:26:10 -0200

    syncing results

--- python/parser.py
+++ python/parser.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 df = pd.read_csv("dadosBrutos.txt",sep=';', index_col =["Strategy", "nUAV"] )
 
-print(df.groupby(level=["Strategy","nUAV"]).mean())
+print(df.groupby(level=["Strategy","nUAV"]).median())
