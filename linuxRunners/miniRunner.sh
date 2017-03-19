7e9590b5378e7ec1de1055aad9d47bef754b00d0
JustAnOtherGuy <jaog@lac2.inf>
2017-03-19 15:00:53 -0300

    stats from server to dev machine

--- linuxRunners/miniRunner.sh
+++ linuxRunners/miniRunner.sh
@@ -10,6 +10,18 @@ REFRESHRATE=3750000
 N_POI=20
 V2X=V2V
 
+
+echo " "
+echo " ----------------------------------------------------------------------------"
+echo " "
+echo "nPOI = "$N_POI"  strat = "$SNAME"  loop = "$i
+date >> acompanhamento.txt
+echo "nPOI = "$N_POI"  strat = "$SNAME"  loop = "$i >> acompanhamento.txt
+echo " "
+echo " ----------------------------------------------------------------------------"
+echo " "
+
+
 for ((i=1; i<=LOOPS; i++)); do
 
 	for N_UAV in 2 4 8 16
@@ -17,7 +29,13 @@ for ((i=1; i<=LOOPS; i++)); do
 
 		#full: NaiveOrderedMobility NotSoNaiveOrderedMobility ZigZagOverNaiveMobility ZigZagOverNSNMobility KingstonImprovedOverNaiveMobility KingstonImprovedOverNSNMobility
 
-		for SNAME in TSPbasedMobility KingstonImprovedOverNSNMobility
+
+		#forcing TSP run
+		java -cp binaries/bin/. sinalgo.Run -$V2X -project UAV_Surveillance -rounds $ROUNDS -refreshRate $REFRESHRATE -batch exitAfter=true exitAfter/Rounds=$ROUNDS exitOnTerminationInGUI=true AutoStart=true outputToConsole=false extendedControl=false -gen $N_UAV UAV_Surveillance:UAVnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=UAV_Surveillance:TSPbasedMobility  R=ReliableDelivery -gen 1 UAV_Surveillance:GSnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery -gen $N_POI UAV_Surveillance:POInode UAV_Surveillance:PoiDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery
+
+
+
+		for SNAME in KingstonImprovedOverNSNMobility
 		do
 
 			echo " "
@@ -52,3 +70,6 @@ for ((i=1; i<=LOOPS; i++)); do
 	done
 
 done
+
+
+echo "terminou legal"
