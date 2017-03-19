c7f078ae1b0c4dfb682757076e5f25287ab46272
brunoolivieri <bruno@olivieri.com.br>
2017-03-19 15:22:17 -0300

    Preparing new evaluations to run.
    #windowsbytecodes

--- linuxRunners/miniRunner.sh
+++ linuxRunners/miniRunner.sh
@@ -2,7 +2,7 @@
 
 cd ..
 
-LOOPS=30
+LOOPS=10
 ROUNDS=3750000
 REFRESHRATE=3750000
 
@@ -11,57 +11,61 @@ N_POI=20
 V2X=V2V
 
 
-echo " "
-echo " ----------------------------------------------------------------------------"
-echo " "
-echo "nPOI = "$N_POI"  strat = "$SNAME"  loop = "$i
-date >> acompanhamento.txt
-echo "nPOI = "$N_POI"  strat = "$SNAME"  loop = "$i >> acompanhamento.txt
-echo " "
-echo " ----------------------------------------------------------------------------"
-echo " "
 
 
 for ((i=1; i<=LOOPS; i++)); do
 
-	for N_UAV in 2 4 8 16
+	for N_POI in 20 40 60 80 100
 	do
 
 		#full: NaiveOrderedMobility NotSoNaiveOrderedMobility ZigZagOverNaiveMobility ZigZagOverNSNMobility KingstonImprovedOverNaiveMobility KingstonImprovedOverNSNMobility
 
 
 		#forcing TSP run
-		java -cp binaries/bin/. sinalgo.Run -$V2X -project UAV_Surveillance -rounds $ROUNDS -refreshRate $REFRESHRATE -batch exitAfter=true exitAfter/Rounds=$ROUNDS exitOnTerminationInGUI=true AutoStart=true outputToConsole=false extendedControl=false -gen $N_UAV UAV_Surveillance:UAVnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=UAV_Surveillance:TSPbasedMobility  R=ReliableDelivery -gen 1 UAV_Surveillance:GSnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery -gen $N_POI UAV_Surveillance:POInode UAV_Surveillance:PoiDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery
+		#java -cp binaries/bin/. sinalgo.Run -$V2X -project UAV_Surveillance -rounds $ROUNDS -refreshRate $REFRESHRATE -batch exitAfter=true exitAfter/Rounds=$ROUNDS exitOnTerminationInGUI=true AutoStart=true outputToConsole=false extendedControl=false -gen $N_UAV UAV_Surveillance:UAVnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=UAV_Surveillance:TSPbasedMobility  R=ReliableDelivery -gen 1 UAV_Surveillance:GSnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery -gen $N_POI UAV_Surveillance:POInode UAV_Surveillance:PoiDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery
 
 
 
 		for SNAME in KingstonImprovedOverNSNMobility
 		do
 
+
 			echo " "
 			echo " ----------------------------------------------------------------------------"
 			echo " "
-			echo " nPOI = "$N_POI"  strat = "$SNAME"  i = "$i 
+			echo "nPOI = "$N_POI"  strat = "$SNAME"  loop = "$i
 			date >> acompanhamento.txt
-			echo "nPOI = "$N_POI"  strat = "$SNAME"  i = "$i >> acompanhamento.txt
+			echo "nPOI = "$N_POI"  strat = "$SNAME"  loop = "$i >> acompanhamento.txt
 			echo " "
 			echo " ----------------------------------------------------------------------------"
 			echo " "
 
 			echo "dispatching first test do background..."  
-			N_UAV=16
+			N_UAV= $(echo "$N_POI / 2" | bc -l )  		
+			echo "" 
+			echo "     despachando POI= $N_POI e UAV= $N_UAV "
+			echo ""
 			nohup java -cp binaries/bin/. sinalgo.Run -$V2X -project UAV_Surveillance -rounds $ROUNDS -refreshRate $REFRESHRATE -batch exitAfter=true exitAfter/Rounds=$ROUNDS exitOnTerminationInGUI=true AutoStart=true outputToConsole=false extendedControl=false -gen $N_UAV UAV_Surveillance:UAVnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=UAV_Surveillance:$SNAME R=ReliableDelivery -gen 1 UAV_Surveillance:GSnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery -gen $N_POI UAV_Surveillance:POInode UAV_Surveillance:PoiDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery &
 
 			echo "dispatching second test do background..."  
-			N_UAV=2
+			N_UAV=$(echo "$N_POI / 4" | bc -l ) 
+			echo "" 
+			echo "     despachando POI= $N_POI e UAV= $N_UAV "
+			echo ""
 			nohup java -cp binaries/bin/. sinalgo.Run -$V2X -project UAV_Surveillance -rounds $ROUNDS -refreshRate $REFRESHRATE -batch exitAfter=true exitAfter/Rounds=$ROUNDS exitOnTerminationInGUI=true AutoStart=true outputToConsole=false extendedControl=false -gen $N_UAV UAV_Surveillance:UAVnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=UAV_Surveillance:$SNAME R=ReliableDelivery -gen 1 UAV_Surveillance:GSnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery -gen $N_POI UAV_Surveillance:POInode UAV_Surveillance:PoiDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery &
 			
 			echo "dispatching third test do background..."  		
-			N_UAV=4
+			N_UAV=$(echo "$N_POI / 10" | bc -l ) 
+			echo "" 
+			echo "     despachando POI= $N_POI e UAV= $N_UAV "
+			echo ""
 			nohup java -cp binaries/bin/. sinalgo.Run -$V2X -project UAV_Surveillance -rounds $ROUNDS -refreshRate $REFRESHRATE -batch exitAfter=true exitAfter/Rounds=$ROUNDS exitOnTerminationInGUI=true AutoStart=true outputToConsole=false extendedControl=false -gen $N_UAV UAV_Surveillance:UAVnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=UAV_Surveillance:$SNAME R=ReliableDelivery -gen 1 UAV_Surveillance:GSnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery -gen $N_POI UAV_Surveillance:POInode UAV_Surveillance:PoiDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery &
 			
 			echo "dispatching fourth test do run here..."  
-			N_UAV=8		
+			N_UAV= $(echo "$N_POI /20" | bc -l )  
+			echo "" 
+			echo "     despachando POI= $N_POI e UAV= $N_UAV "
+			echo ""
 			java -cp binaries/bin/. sinalgo.Run -$V2X -project UAV_Surveillance -rounds $ROUNDS -refreshRate $REFRESHRATE -batch exitAfter=true exitAfter/Rounds=$ROUNDS exitOnTerminationInGUI=true AutoStart=true outputToConsole=false extendedControl=false -gen $N_UAV UAV_Surveillance:UAVnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=UAV_Surveillance:$SNAME R=ReliableDelivery -gen 1 UAV_Surveillance:GSnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery -gen $N_POI UAV_Surveillance:POInode UAV_Surveillance:PoiDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery
 			
 			
