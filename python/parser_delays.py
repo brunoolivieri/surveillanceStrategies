75bee47347427024b61362e874cb6c2b19cdd9b5
JustAnOtherGuy <jaog@lac2.inf>
2017-04-18 06:48:09 -0300

    picking up results

--- python/parser_delays.py
+++ python/parser_delays.py
@@ -1,24 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv("stats_delays_KIMPOverNSN.txt",sep=';', index_col =["Strategy", "nUAV", "nPOIs"] )
+df = pd.read_csv("stats_delays_.txt",sep=';', index_col =["Strategy", "nUAV", "nPOIs"] )
 
-df.groupby(level=["Strategy","nUAV","nPOIs"]).median().to_csv("DelayKIMP_MEDIAN.txt", sep=';', encoding='utf-8')
-df.groupby(level=["Strategy","nUAV","nPOIs"]).mean().to_csv("DelayKIMP_MEAN.txt", sep=';', encoding='utf-8')
-df.groupby(level=["Strategy","nUAV","nPOIs"]).describe().to_csv("DelayKIMP_DESCRIBE.txt", sep=';', encoding='utf-8')
-
-
-df2 = pd.read_csv("stats_delays_TSPbased.txt",sep=';', index_col =["Strategy", "nUAV", "nPOIs"] )
-
-df2.groupby(level=["Strategy","nUAV","nPOIs"]).median().to_csv("DelayTSP_MEDIAN.txt", sep=';', encoding='utf-8')
-df2.groupby(level=["Strategy","nUAV","nPOIs"]).mean().to_csv("DelayTSP_MEAN.txt", sep=';', encoding='utf-8')
-df2.groupby(level=["Strategy","nUAV","nPOIs"]).describe().to_csv("DelayTSP_DESCRIBE.txt", sep=';', encoding='utf-8')
-
-
-
-df3 = pd.read_csv("stats_delays_ZigZagOverNSN.txt",sep=';', index_col =["Strategy", "nUAV", "nPOIs"] )
-
-df3.groupby(level=["Strategy","nUAV","nPOIs"]).median().to_csv("DelayZZ_MEDIAN.txt", sep=';', encoding='utf-8')
-df3.groupby(level=["Strategy","nUAV","nPOIs"]).mean().to_csv("DelayZZ_MEAN.txt", sep=';', encoding='utf-8')
-df3.groupby(level=["Strategy","nUAV","nPOIs"]).describe().to_csv("DelayZZ_DESCRIBE.txt", sep=';', encoding='utf-8')
-
+df.groupby(level=["Strategy","nUAV","nPOIs"]).median().to_csv("Delay_MEDIAN.txt", sep=';', encoding='utf-8')
+df.groupby(level=["Strategy","nUAV","nPOIs"]).mean().to_csv("Delay_MEAN.txt", sep=';', encoding='utf-8')
+df.groupby(level=["Strategy","nUAV","nPOIs"]).describe().to_csv("Delay_DESCRIBE.txt", sep=';', encoding='utf-8')
 
