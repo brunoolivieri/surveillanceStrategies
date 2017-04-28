2729bdefecae9904df4adbe99154a331b1373085
JustAnOtherGuy <jaog@lac2.inf>
2017-04-28 10:59:21 -0300

    results

--- linuxRunners/2017-04-27 - 30 New Map - 100POIS - 10UAV - meiahora TSP - 10hSim 1core.sh
+++ linuxRunners/2017-04-27 - 30 New Map - 100POIS - 10UAV - meiahora TSP - 10hSim 1core.sh
@@ -10,31 +10,6 @@ REFRESHRATE=360000
 #N_POI=100
 V2X=V2V
 
-
-
-for ((i=1; i<=LOOPS; i++)); do
-
-	for N_POI in 100
-	do
-		for SNAME in ZigZagOverNSNMobility 
-		do
-			date >> acompanhamento.txt
-			echo "nPOI = "$N_POI"  strat = "$SNAME"  loop = "$i" N_POI = "$N_POI >> acompanhamento.txt
-		
-			N_UAV=$(echo "($N_POI /2)/1" | bc  )  
-			N_UAV=10
-			echo "" 
-			echo "     despachando POI= $N_POI e UAV= $N_UAV "
-			echo ""
-			java -cp binaries/bin/. sinalgo.Run -V2V -LOADFILE savedDistributions/30000x20000x100POIs/$i.txt -project UAV_Surveillance -rounds $ROUNDS -refreshRate $REFRESHRATE -batch exitAfter=true exitAfter/Rounds=$ROUNDS exitOnTerminationInGUI=true AutoStart=true outputToConsole=false extendedControl=false -gen $N_UAV UAV_Surveillance:UAVnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=UAV_Surveillance:$SNAME R=ReliableDelivery -gen 1 UAV_Surveillance:GSnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery -gen $N_POI UAV_Surveillance:POInode UAV_Surveillance:PoiDistributionFromFile C=UDG I=NoInterference M=NoMobility R=ReliableDelivery
-			
-			
-		done
-
-	done
-
-done
-
 N_POI=100
 
 for SNAME in TSPbasedMobility
@@ -49,6 +24,9 @@ do
 			echo "nPOI = "$N_POI"  strat = "$SNAME"  loop = "$i" N_POI = "$N_POI >> acompanhamento.txt
 
 			java -cp binaries/bin/. sinalgo.Run -V2V -LOADFILE savedDistributions/30000x20000x100POIs/$i.txt -project UAV_Surveillance -rounds $ROUNDS -refreshRate $REFRESHRATE -batch exitAfter=true exitAfter/Rounds=$ROUNDS exitOnTerminationInGUI=true AutoStart=true outputToConsole=false extendedControl=false -gen $N_UAV UAV_Surveillance:UAVnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=UAV_Surveillance:$SNAME R=ReliableDelivery -gen 1 UAV_Surveillance:GSnode UAV_Surveillance:UavNearGsDistribution C=UDG I=NoInterference M=NoMobility R=ReliableDelivery -gen $N_POI UAV_Surveillance:POInode UAV_Surveillance:PoiDistributionFromFile C=UDG I=NoInterference M=NoMobility R=ReliableDelivery
+
+	
+
 
 			done
 
--- python/trata_saida2.sh
+++ python/trata_saida2.sh
@@ -5,7 +5,7 @@ sed 's/%//g' $1 > tmp.txt
 sed 's/MiliSegs//g' tmp.txt > tmp2.txt
 sed 's/_TSP_thread//g' tmp2.txt > tmp3.txt
 
-echo "Strategy;nPOIs;nUAV;SucessTax;V2V_range;nRounds;dimX;simumationTimeMS;TSP_threads;maxData;minData;globalAvgDelay;nMsgs;tourSize;" > dadosBrutos.txt
+echo "Strategy;nPOIs;nUAV;SucessTax;V2V_range;nRounds;dimX;simumationTimeMS;TSP_threads;maxData;minData;globalAvgDelay;nMsgs;tourSize" > dadosBrutos.txt
 
 
 
